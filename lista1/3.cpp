cc09c8d2165b91d182d2a3fb6d54e9f9046e897c
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-06-11 18:33:01 -0400

    beta

--- lista1/3.cpp
+++ lista1/3.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 class matriz
 {
+    friend void somaMatriz(matriz, matriz);
+    friend void multMatriz(matriz, matriz);
 
 public:
     matriz();
@@ -11,8 +13,6 @@ public:
     void setmatriz();
     void getmatriz();
     void inversa();
-    // metodo soma
-    // metodo multiplicação
 
 private:
     float (*m)[2];
@@ -64,6 +64,8 @@ void matriz::inversa()
     inversa[0][1] = m[0][1] * -1;
     inversa[1][0] = m[1][0] * -1;
 
+    cout << "Inversa: " << endl;
+
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < 2; j++)
@@ -73,7 +75,71 @@ void matriz::inversa()
         cout << "\n";
     }
 
+    cout << "\n";
+
     delete[] inversa;
+}
+
+void somaMatriz(matriz a, matriz b)
+{
+    float(*soma)[2] = new float[2][2];
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            soma[i][j] = a.m[i][j] + b.m[i][j];
+        }
+    }
+
+    cout << "Soma: " << endl;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << soma[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    delete[] soma;
+}
+
+void multMatriz(matriz a, matriz b)
+{
+    float(*multi)[2] = new float[2][2];
+
+    // ainda a implementar
+    /*for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+        }
+    } */
+
+    // provisório
+    multi[0][0] = (a.m[0][0] * b.m[0][0]) + (a.m[0][1] * b.m[1][0]);
+    multi[0][1] = (a.m[0][0] * b.m[0][1]) + (a.m[0][1] * b.m[1][1]);
+    multi[1][0] = (a.m[1][0] * b.m[0][0]) + (a.m[1][1] * b.m[1][0]);
+    multi[1][1] = (a.m[1][0] * b.m[0][1]) + (a.m[1][1] * b.m[1][1]);
+    
+    cout << "Multiplicacao: " << endl;
+
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << multi[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    cout << "\n";
+
+    delete[] multi;
 }
 
 int main(void)
@@ -82,8 +148,11 @@ int main(void)
     matriz q;
 
     p.setmatriz();
-    p.getmatriz();
+    q.setmatriz();
     p.inversa();
+    q.inversa();
+    multMatriz(p, q);
+    somaMatriz(p, q);
 
     return 0;
 }
