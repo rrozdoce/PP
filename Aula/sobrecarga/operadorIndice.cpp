4cf08da8b6da85ac9083c8b9251c7f7a8cba1bdb
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-06-27 18:05:20 -0400

    att sobrecarga[]

--- Aula/sobrecarga/operadorIndice.cpp
+++ Aula/sobrecarga/operadorIndice.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-// sobrecarga do operador indice
+// sobrecarga do operador indice []
+// Exemplo simples
 
 class Vetor
 {
@@ -26,10 +27,17 @@ public:
         // alocar espaço
 
         vet = new int[max];
+        vet_pos = new int[max];
+
+        for (int i = 0; i < max; i++)
+        {
+            vet_pos[i] = 0;
+        }
     }
     ~Vetor()
     {
         delete[] vet;
+        delete[] vet_pos;
     }
 
     bool inserir(int e, int pos)
@@ -37,6 +45,7 @@ public:
         if (pos < max && pos >= 0)
         {
             vet[pos] = e;
+            vet_pos[pos] = 1;
             return true;
         }
         return false;
@@ -49,11 +58,28 @@ public:
             cerr << "Erro: acesso invalido ao vetor" << endl;
             exit(1);
         }
+        else if (vet_pos[i] == 0)
+        {
+            cerr << "Erro: nessa posicao nao existe elemento." << endl;
+            exit(1);
+        }
         return vet[i];
     }
 
+    int tam()
+    {
+        int cont = 0;
+
+        for (int i = 0; i < max; i++)
+        {
+            if (vet_pos[i] == 1)
+                cont++;
+        }
+        return cont;
+    }
+
 private:
-    int *vet;
+    int *vet, *vet_pos;
     int max;
 };
 
@@ -73,6 +99,10 @@ int main(int argc, char const *argv[])
         cout << "Elemento inserir com sucesso!\n";
     else
         cout << "Erro ao inserir o elemento.\n";
+
+    cout << "Primerio elemento: " << v[0] << endl;
+    cout << "Terceiro elemento: " << v[1] << endl;
+    cout << "Segundo elemento: " << v[2] << endl;
 
     return 0;
 }
