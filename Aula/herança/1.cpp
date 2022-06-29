2aede5d64346aa731a60f3366225e8a8830f9cb6
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-06-28 21:42:25 -0400

    add herança

--- Aula/herança/1.cpp
+++ Aula/herança/1.cpp
@@ -1,10 +1,80 @@
-#include <iostream>
+//
+// Created by XSpat on 6/28/2022.
+//
 
-// herança
+#include "iostream"
+#include "string.h"
+
+// Heranças U_U
+
+/*
+ Class Animal
+
+ Class Cachorro
+
+ Cachoroo é classe filha
+ Animal é classe mãe(classe base)
+ Cachorro é classe filha
+ Cachorro herda caracteristicas de Animal
+ Cachorro é um Animal
+
+ */
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+class Animal
 {
+protected:
+     char* nome;
+public:
+    Animal(const char* nome)
+    {
+        cout << "Construindo Animal... " << endl;
+        this->nome = new char[strlen(nome) + 1];
+        strcpy(this->nome, nome);
+    }
+    ~Animal()
+    {
+       delete[] nome;
+       nome = 0;
+    }
+
+    char *getNome() const {
+        return nome;
+    }
+
+
+};
+
+class Cachorro : public Animal
+{
+protected:
+    int idade;
+public:
+    Cachorro(const char* nome) : Animal(nome)
+    {
+        cout << "Construindo Cachorro... " << endl;
+        idade = 0;
+    }
+    int getIdade()
+    {
+        return idade;
+    }
+
+    void setIdade(int idade) {
+        this->idade = idade;
+    }
+};
+
+using namespace std;
+
+int main(void)
+{
+    Cachorro c("Yankee");
+    c.setIdade(5);
+
+    cout << "Nome: " << c.getNome() << endl;
+    cout << "Idade: " << c.getIdade() << endl;
+
     return 0;
 }
