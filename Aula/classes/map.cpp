d9e0582cb624b12cedd1610677d258df99d1da5c
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-07-19 15:13:21 -0400

    att

--- Aula/classes/map.cpp
+++ Aula/classes/map.cpp
@@ -1,7 +1,69 @@
 #include <iostream>
 #include <map>
+using namespace std;
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+    map<int, string> mapa;
+
+    mapa[1] = "joao";
+    mapa[2] = "maria";
+    mapa[3] = "pedro";
+
+    // alterando o valor associado a uma chave
+    mapa.at(1) = "carol";
+
+    if (mapa.empty())
+        cout << "Mapa vazio!!\n"
+             << endl;
+    else
+        cout << "Mapa NAO vazio!!\n"
+             << endl;
+
+    if (mapa.count(4) > 0)
+        cout << "4 eh elemento do mapa\n";
+    else
+        cout << "4 NAO eh elemento do mapa\n";
+
+    // mostrando todos os elementos
+    map<int, string>::iterator it;
+    for (it = mapa.begin(); it != mapa.end(); ++it)
+        cout << "A chave " << it->first << " => " << it->second << endl;
+
+    it = mapa.find(2);
+    mapa.erase(it);
+
+    // verificando se uma chave existe
+    if (mapa.find(2) == mapa.end())
+        cout << "\nChave 2 NAO existe!\n\n";
+    else
+        cout << "\nChave 2 existe!\n\n";
+
+    it = mapa.begin();
+    mapa.insert(pair<int, string>(2, "maria"));
+
+    // mostrando todos os elementos
+    for (it = mapa.begin(); it != mapa.end(); ++it)
+        cout << "A chave " << it->first << " => " << it->second << endl;
+
+    // utilizando multimap
+    multimap<int, string> mm;
+
+    mm.insert(pair<int, string>(1, "python 2"));
+    mm.insert(pair<int, string>(1, "python 3"));
+    mm.insert(pair<int, string>(3, "C"));
+    mm.insert(pair<int, string>(4, "C++"));
+    mm.insert(pair<int, string>(4, "ruby"));
+
+    multimap<int, string>::iterator it_low, it_up, it2;
+
+    it_low = mm.lower_bound(2);
+    it_up = mm.upper_bound(4);
+
+    cout << endl;
+
+    for (it2 = it_low; it2 != it_up; ++it2)
+        cout << it2->first << " => " << it2->second << endl;
+
     return 0;
 }
--- Aula/classes/queue.cpp
+++ Aula/classes/queue.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
+#include <queue>
+
+using namespace std;
+
+// queue
 
 int main(int argc, char const *argv[])
 {
+    queue<int> fila;
+
+    // inserindo na fila
+    fila.push(10);
+    fila.push(20);
+    fila.push(30);
+    // fila: 10 20 30
+
+    // mostrando o ultimo elemento
+    cout << "\nUltimo elemento: " << fila.back() << endl;
+
+    if (fila.empty())
+    {
+        cout << "Fila vazia!!\n";
+    }
+    else
+    {
+        cout << "Fila NAO vazia!!\n";
+    }
+
+    cout << "\nPrimeiro elemento: " << fila.front() << endl;
+
+    fila.pop();
+
+    cout << "\nMostrando todos os elementos: ";
+    while(!fila.empty())
+    {
+        int e = fila.front();
+        cout << e << " ";
+        fila.pop();
+    }
+
+    // inserindo novamente na fila
+    fila.push(10);
+    fila.push(20);
+    fila.push(30);
+
+    cout << "\n\nTamanho da fila: " << fila.size() << endl;
+    
+
     return 0;
 }
