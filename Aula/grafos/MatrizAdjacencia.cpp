b8d42279a9a40b10e9f1d19754563afac9b38fb8
rrozdoce <felipefelipevilhalva@gmail.comm>
2022-07-21 21:41:47 -0400

    att

--- Aula/grafos/MatrizAdjacencia.cpp
+++ Aula/grafos/MatrizAdjacencia.cpp
@@ -1,8 +1,27 @@
+// Grafo - Matriz de adjacência
+
 #include <iostream>
 
 using namespace std;
 
-int main(void)
+int grafo[5][5] =
 {
-    return 0;
+	{0, 0, 1, 0, 0},
+	{0, 0, 1, 0, 0},
+	{1, 1, 0, 1, 1},
+	{0, 0, 1, 0, 1},
+	{0, 0, 1, 1, 0}
+};
+
+bool tem_ligacao(int v1, int v2)
+{
+	if(grafo[v1][v2])
+		return true;
+	return false;
+}
+
+int main(int argc, char *argv[])
+{
+	cout << tem_ligacao(0, 2) << endl;
+	return 0;
 }
